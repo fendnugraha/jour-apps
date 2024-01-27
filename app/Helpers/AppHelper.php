0e50e6d5da50a6076cc5983855dc0f42e7939013
Fend <fendnugraha@Fends-MacBook-Pro.local>
2024-01-27 23:24:32 +0700

    add custom number minus

--- app/Helpers/AppHelper.php
+++ app/Helpers/AppHelper.php
@@ -2,10 +2,15 @@
 function custom_number($number)
 {
     $suffix = '';
+    $sign = ($number < 0) ? '-' : '';
+
+    $number = abs($number); // Convert to absolute value for calculation
+
     if ($number >= 1000) {
         $units = ['', 'K', 'M', 'B', 'T'];
         $suffix = $units[floor(log10($number) / 3)];
         $number = round($number / pow(1000, $suffix ? array_search($suffix, $units) : 0), 2);
     }
-    return $number . $suffix;
+
+    return $sign . $number . $suffix;
 }
